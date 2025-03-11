205c939ea6473ad0082d9620a362ddd51dc77358
Admin <2216528769@qq.com>
2025-03-11 19:29:15 +0800

    12312

--- OuseBlog/Program.cs
+++ OuseBlog/Program.cs
@@ -6,13 +6,14 @@ using OuseBlog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddMudServices();
+
 builder.RootComponents.Add<App>("#app");
 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMudServices();
 
 builder.Services.AddMudMarkdownServices();
 
