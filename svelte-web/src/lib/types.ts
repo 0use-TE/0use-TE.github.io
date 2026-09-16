4ec6cd7990f0b2ea745592ee5423579643d78203
ouse <2216528769@qq.com>
2026-09-16 22:57:51 +0800

    q

--- svelte-web/src/lib/types.ts
+++
@@ -1,5 +0,0 @@
-export interface BlogPost {
-  title: string;
-  path: string;
-  category: string;
-}
--- svelte-web/src/routes/code/[pet]/+page.ts
+++
@@ -1,5 +0,0 @@
-import type { EntryGenerator } from './$types';
-
-export const entries: EntryGenerator = () => {
-	return [{ pet: 'code' }, { pet: 'jimi' }];
-};
